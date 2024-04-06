ace580e53b4a507e1352216a98c9928c8a1e277d
HajarIH <languages0701@gmail.com>
2024-04-06 22:16:41 +0000

    pack

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,17 +6,17 @@ from fabric.api import local
 
 
 def do_pack():
-    """Create a tar gzipped archive of the directory web_static."""
-    dt = datetime.utcnow()
-    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
-                                                         dt.month,
-                                                         dt.day,
-                                                         dt.hour,
-                                                         dt.minute,
-                                                         dt.second)
-    if os.path.isdir("versions") is False:
-        if local("mkdir -p versions").failed is True:
-            return None
-    if local("tar -cvzf {} web_static".format(file)).failed is True:
+    """Generates a .tgz archive from the contents of the web_static folder."""
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    archive_name = 'web_static_{}.tgz'.format(timestamp)
+    archive_path = os.path.join('versions', archive_name)
+
+    if not os.path.exists('versions'):
+        local('mkdir -p versions')
+
+    result = local('tar -cvzf {} web_static'.format(archive_path))
+
+    if result.failed:
         return None
-    return file
+    else:
+        return archive_path

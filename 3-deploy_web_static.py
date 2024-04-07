a49e27ce29dcac04b1f86c3027cd0cefd211fc1f
HajarIH <languages0701@gmail.com>
2024-04-07 14:54:11 +0000

    archive

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -32,23 +32,23 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-     if exists(archive_path) is False:
-         return False
-     try:
-         file_n = archive_path.split("/")[-1]
-         no_ext = file_n.split(".")[0]
-         path = "/data/web_static/releases/"
-         put(archive_path, '/tmp/')
-         run('mkdir -p {}{}/'.format(path, no_ext))
-         run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-         run('rm /tmp/{}'.format(file_n))
-         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-         run('rm -rf {}{}/web_static'.format(path, no_ext))
-         run('rm -rf /data/web_static/current')
-         run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-         return True
-     except:
-         return False
+    if exists(archive_path) is False:
+        return False
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
 
 def deploy():
     """Create and distribute an archive to a web server."""
